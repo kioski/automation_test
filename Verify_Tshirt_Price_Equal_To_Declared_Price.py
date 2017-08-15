11aa9049e98ff80dee9717e33120f67aff87e1d5
Emman-PC\Emman <kioskigal@gmail.com>
2017-08-15 20:54:48 +0800

    Added changes for verify prices

--- Verify_Tshirt_Price_Equal_To_Declared_Price.py
+++ Verify_Tshirt_Price_Equal_To_Declared_Price.py
@@ -11,7 +11,7 @@ class Verify_Tshirt_Price_Equal_To_Declared_Price(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
 
-    def test_Open_Website(self):
+    def test_Price_Compare(self):
 
         self.driver.get("https://pupfanatic.com/")
         self.assertTrue("PupFanatic | ", self.driver.title)
@@ -23,24 +23,22 @@ class Verify_Tshirt_Price_Equal_To_Declared_Price(unittest.TestCase):
 
 
         # Assert Lower Price
-        USD24_95, USD14_95 = all_options[1].text, all_options[36].text
         for list_1 in range(1,121, 5):
             PRICE_1 = all_options[list_1].text
-            if PRICE_1 == USD24_95:
-                self.assertEqual(PRICE_1, USD24_95)
+            if PRICE_1 == "$24.95":
+                self.assertEqual(PRICE_1, "$24.95")
             else:
-                self.assertEqual(PRICE_1, USD14_95)
+                self.assertEqual(PRICE_1, "$14.95")
 
         # Assert Upper Price
-        USD30, USD19_95, USD29_50 = all_options[3].text, all_options[38].text, all_options[58].text   
         for list_2 in range(3,123,5):
             PRICE_2 = all_options[list_2].text
-            if PRICE_2 == USD30:
-                self.assertEqual(PRICE_2, USD30)
-            elif PRICE_2 == USD19_95:
-                self.assertEqual(PRICE_2, USD19_95)
+            if PRICE_2 == "$30.00":
+                self.assertEqual(PRICE_2, "$30.00")
+            elif PRICE_2 == "$19.95":
+                self.assertEqual(PRICE_2, "$19.95")
             else:
-                self.assertEqual(PRICE_2, USD29_50)
+                self.assertEqual(PRICE_2, "$29.50")
 
     def tearDown(self):
         self.driver.close()
