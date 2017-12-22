88325e03e7733c7a3e3dfcc3c489e622266b7d77
Emman-PC\Emman <kioskigal@gmail.com>
2017-12-22 09:42:14 +0800

    c# test scripts

---
+++ PupfanaticTesting/PupfanaticTesting/Test/Pupfanatic_FrontPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+
+namespace PupfanaticTesting
+{
+    [TestFixture]
+    public class PupfanaticTesting
+    {
+        [Test]
+        public void Verify_Tshirt_Price_Equal_To_Declared_Price()
+        {
+
+        }
+    }
+}
---
+++ PupfanaticTesting/PupfanaticTesting/TestAction/Pupfanatic_FrontPage_TestAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+
+namespace PupfanaticTesting_TestAction
+{
+
+    public class UnitTest1
+    {
+
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
